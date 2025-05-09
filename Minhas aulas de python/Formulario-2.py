6bca5cc29ec51f8c4b7b18f5eb0e2018c5d02607
DAVID DA SILVA DOS SANTOS <zmaximus.liga@gmail.com>
2025-05-08 23:11:53 -0300

    novo projeto

---
+++ Minhas aulas de python/Formulario-2.py
@@ -0,0 +1,10 @@
+# formulário de ficha de protudo e cliente
+
+cliente = str(input('Digite o nome do cliente: '))
+data = int(input('digite dia, mes e ano: '))
+produto = str(input('Digite o nome do produto: '))
+serie = str(input('Digite o numero de serie: '))
+estadoproduto = str(input('Digite o estado do produto: '))
+itensdoproduto = str(input('Digite os itens do produto: '))
+defeitoreclamado = str(input('Digite o defeito reclamado: '))
+defeitoencontrado = str(input('Digite o defeito encontrado: '))
